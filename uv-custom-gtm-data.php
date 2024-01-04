ccb33cd85abefc6d8cee0f1445803f1c92162c21
Simon Schuhmacher <simon@uvoglu.com>
2024-01-04 10:47:13 +0100

    add plugin header

---
+++ uv-custom-gtm-data.php
@@ -0,0 +1,13 @@
+<?php
+
+/*
+ * Plugin Name:       Custom GTM Data
+ * Plugin URI:        https://github.com/uvoglu/wp-custom-gtm-data
+ * Description:       Push custom post data to the Google Tag Manager Data Layer
+ * Version:           0.1.0
+ * Author:            Simon Schuhmacher
+ * Author URI:        https://uvoglu.com
+ * License:           GPL v2 or later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       uv-custom-gtm-data
+ */
